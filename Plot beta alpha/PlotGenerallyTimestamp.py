90d0b3413fe2f877ec092d10441a46af3b33e48c
Yannick <nzalikyannick01@gmail.com>
2024-05-27 17:29:15 +0200

    Changes to be kept

--- Plot beta alpha/PlotGenerallyTimestamp.py
+++ Plot beta alpha/PlotGenerallyTimestamp.py
@@ -1,41 +1,11 @@
+import os
+
 import matplotlib.pyplot as plt
 import numpy as np
 import json
 import re
 import pandas as pd
 from datetime import datetime
-
-json_files1 = [
-    '../data/cpu/teastore-webui-8665fbfd74-dcw7r.json',
-    '../data/cpu/teastore-registry-f6dcdf97c-lr26h.json',
-    '../data/cpu/teastore-recommender-79c58f6556-88jb8.json',
-    '../data/cpu/teastore-persistence-668667c675-jh6dz.json',
-    '../data/cpu/teastore-image-84cc4fd468-mlrmk.json',
-    '../data/cpu/teastore-db-5847859bf8-r52pb.json',
-    '../data/cpu/teastore-auth-5767dbc6b-7h4z7.json'
-]
-
-json_files2 = [
-    '../data/memory/teastore-webui-8665fbfd74-dcw7r.json',
-    '../data/memory/teastore-registry-f6dcdf97c-lr26h.json',
-    '../data/memory/teastore-recommender-79c58f6556-88jb8.json',
-    '../data/memory/teastore-persistence-668667c675-jh6dz.json',
-    '../data/memory/teastore-image-84cc4fd468-mlrmk.json',
-    '../data/memory/teastore-db-5847859bf8-r52pb.json',
-    '../data/memory/teastore-auth-5767dbc6b-7h4z7.json'
-]
-
-json_files3 = [
-    '../data3/cpu/teastore-webui-59f448d7f5-8z9cs.json',
-    '../data3/cpu/teastore-registry-69c86867cd-mxccl.json',
-    '../data3/cpu/teastore-recommender-6b67599fb9-7g8qv.json',
-    '../data3/cpu/teastore-persistence-7d6bcb6b96-jcl4d.json',
-    '../data3/cpu/teastore-image-6b9796d7c7-tsq8h.json',
-    '../data3/cpu/teastore-db-7685d7b587-bl4vl.json',
-    '../data3/cpu/teastore-auth-8877cbcc9-5k2jz.json'
-]
-
-
 def smooth(values, w_size=5):
     new_values = []
     for i in range(len(values)):
@@ -52,12 +22,7 @@ def plot_json(file_name, label):
 
     timestamps = np.array([int(ts) for ts, _ in datas])
 
-    #given_value = 1716457765.096  # Replace with your desired value
-    #given_value = 1716462610.14867  # Replace with your desired value
-    #given_value = 1716515990.615 # Replace with your desired value
     given_value = 1716782470.186 # Replace with your desired value
-
-    #timestamps = np.array([int(ts) for ts, _ in datas])
 
     greater_than_value = timestamps[timestamps > given_value]
     less_than_value = timestamps[timestamps <= given_value]
@@ -69,16 +34,8 @@ def plot_json(file_name, label):
     greater_than_valueReduce = greater_than_value
     last_ten_valuesReduce = last_ten_values
 
-    #lissageValues = smooth(last_ten_valuesReduce)
     lissageValues = last_ten_valuesReduce
 
-    # Plot the graph
-    #plt.plot(timestamps, values, label=label)
-    #plt.plot(greater_than_valueReduce, last_ten_valuesReduce, label=label)
-    print("le max")
-    print(file_name)
-    print(max(lissageValues))
-    print(len(lissageValues))
     plt.plot(greater_than_valueReduce, lissageValues, label=label)
     return greater_than_valueReduce
 
@@ -89,11 +46,16 @@ plt.figure(figsize=(10, 16))
 # Plot the first set of data
 plt.subplot(4, 1, 1)
 all_timestamps = []
-for file_name in json_files1:
-    file_parts = file_name.split("/")
+
+directory = '../data/cpu'
+for file_name in os.listdir(directory):
+    file_path = os.path.join(directory, file_name)
+    print(file_path)
+    #for file_name in json_files1:
+    file_parts = file_path.split("/")
     last_part = (file_parts[-1]).split(".")[0]
     result = re.split(r'-\d+', last_part)[0]
-    timestamps = plot_json(file_name, result)
+    timestamps = plot_json(file_path, result)
     all_timestamps.append(timestamps)
 
 # Concatenate all timestamps
@@ -102,10 +64,6 @@ all_timestamps = np.concatenate(all_timestamps)
 # Calculate the start and end times
 start_time = min(all_timestamps)
 end_time = max(all_timestamps)
-
-# Calculate the nearest 20-second intervals for start and end times
-#start_time = start_time - (start_time % 20)
-#end_time = end_time + (20 - end_time % 20)
 
 # Generate a list of ticks every 20 seconds
 ticks = np.arange(start_time, end_time + 1, 20)
@@ -124,11 +82,16 @@ plt.legend()
 # Plot the second set of data
 plt.subplot(4, 1, 2)
 all_timestamps2 = []
-for file_name in json_files2:
-    file_parts = file_name.split("/")
+
+directory2 = '../data/memory'
+for file_name in os.listdir(directory2):
+    file_path = os.path.join(directory2, file_name)
+    print(file_path)
+    #for file_name in json_files1:
+    file_parts = file_path.split("/")
     last_part = (file_parts[-1]).split(".")[0]
     result = re.split(r'-\d+', last_part)[0]
-    timestamps2 = plot_json(file_name, result)
+    timestamps2 = plot_json(file_path, result)
     all_timestamps2.append(timestamps2)
 
 # Concatenate all timestamps
@@ -137,10 +100,6 @@ all_timestamps2 = np.concatenate(all_timestamps2)
 # Calculate the start and end times
 start_time2 = min(all_timestamps2)
 end_time2 = max(all_timestamps2)
-
-# Calculate the nearest 20-second intervals for start and end times
-#start_time2 = start_time2 - (start_time2 % 20)
-#end_time2 = end_time2 + (20 - end_time2 % 20)
 
 # Generate a list of ticks every 20 seconds
 ticks2 = np.arange(start_time2, end_time2 + 1, 20)
@@ -208,8 +167,6 @@ for item in data["data"]["result"]:
 
 datas = tab
 
-print(tab)
-
 timestamps3 = np.array([int(ts) for ts, _ in datas])
 values3 = [float(value) for _, value in datas]
 
@@ -240,10 +197,6 @@ scaled_seconds = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
 #plt.figure(figsize=(12, 6))
 #plt.plot(scaled_seconds, values, marker='o', linestyle='-', linewidth=2)
 
-print("the array")
-print(scaled_seconds)
-print(values)
-
 plt.xlabel('Time (seconds)')
 plt.ylabel('Number of pods')
 plt.title('Evolution of pods')
@@ -252,18 +205,11 @@ plt.title('Evolution of pods')
 tick_values = [i * 20 for i in range(len(x_values))]
 tick_labels = [str(i * 20) for i in range(len(x_values))]
 
-print("les vleurs")
 print(tick_values)
-print("les labels")
 print(tick_labels)
 plt.xticks(x_values, tick_labels)
 
 plt.tight_layout()
-
-# now = datetime.datetime.now()
-# date_time_str = now.strftime("%Y-%m-%d_%H-%M-%S")
-# file_name = f"cpuMemoryLoadGenerate_{date_time_str}.png"
-# plt.savefig(file_name)
 
 plt.savefig('cpuMemoryLoadGenerate27.png')
 plt.show()
--- drafts/dateToTimestap.py
+++ drafts/dateToTimestap.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
-time_str = "17:42:57.325"
+time_str = "06:01:10.186"
 
 # Assuming today's date for simplicity
-date_str = "2024-05-02"
+date_str = "2024-05-27"
 
 # Combine date and time strings to create a datetime object
 datetime_str = date_str + " " + time_str
